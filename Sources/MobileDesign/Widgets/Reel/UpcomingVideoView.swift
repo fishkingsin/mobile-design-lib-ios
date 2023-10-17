5f853a3dacb5a11769daf3fbffb7e15307213838
Joey Suen <joey_sun@chiefgroup.com.hk>
2023-10-17 22:05:47 +0800

    feat: [iOS] Upcoming Video View
    https://app.clickup.com/t/860rpe4t0

---
+++ Sources/MobileDesign/Widgets/Reel/UpcomingVideoView.swift
@@ -0,0 +1,114 @@
+//
+//  File.swift
+//
+//
+//  Created by Joey Sun on 2023/10/17.
+//
+
+import Foundation
+import SwiftUI
+import Kingfisher
+
+protocol UpcomingItem {
+    var imageURL: String { get }
+    var timeCode: String { get }
+    var headline: String { get }
+    var secCountDown: Int { get }
+}
+
+protocol UpcomingVideoViewEvent {
+    func onClickCancel()
+    func onClickPlay()
+}
+
+struct UpcomingVideoView<Item: UpcomingItem>: View {
+    var item: Item
+    var event: UpcomingVideoViewEvent?
+
+    @State private var secCountDown = 10
+    private let theme: NMGThemeable = ThemeManager.shared.currentTheme
+
+    var body: some View {
+        VStack() {
+            Spacer().frame(height: 11)
+            HStack {
+                Spacer().frame(width: 18)
+                Text("將於")
+                    .font(.system(size: 14))
+                    .foregroundColor(theme.colors.neutralGray50.color)
+                Text("\(secCountDown)")
+                    .font(.system(size: 14))
+                    .foregroundColor(theme.colors.neutralGray2.color)
+                Text("秒後播放")
+                    .font(.system(size: 14))
+                    .foregroundColor(theme.colors.neutralGray50.color)
+                Spacer()
+            }
+            HStack() {
+                Spacer().frame(width: 14)
+                ZStack(alignment: .bottomTrailing) {
+                    KFImage.url(URL(string: item.imageURL))
+                        .resizable()
+                        .frame(width: 144, height: 75)
+                        .background(Color.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 4))
+                    Text(item.timeCode)
+                        .font(.system(size: 12))
+                        .padding(4)
+                        .background(Color(white: 0.5, opacity: 0.8))
+                        .clipShape(RoundedRectangle(cornerRadius: 4))
+                }
+                Text(item.headline)
+                    .font(.system(size: 16))
+                    .foregroundColor(theme.colors.neutralGray2.color)
+                    .lineLimit(3)
+                    .truncationMode(.tail)
+                Spacer().frame(width: 14)
+            }
+            Spacer()
+            HStack() {
+                Spacer().frame(width: 16)
+                Button(action: {
+                    event?.onClickCancel()
+                }){
+                    Text("取消")
+                        .font(.system(size: 16))
+                        .foregroundColor(theme.colors.neutralGray2.color)
+                        .frame(maxWidth: .infinity)
+                        .padding(9)
+                        .background(RoundedRectangle(cornerRadius: 2).stroke(theme.colors.neutralGray2.color))
+                }
+                Spacer().frame(width: 16)
+                Button(action: {
+                    event?.onClickPlay()
+                }){
+                    Text("立即播放")
+                        .font(.system(size: 16))
+                        .foregroundColor(theme.colors.neutralGray90.color)
+                        .frame(maxWidth: .infinity)
+                        .padding(9)
+                        .background(theme.colors.neutralGray2.color)
+                        .clipShape(RoundedRectangle(cornerRadius: 2))
+                }
+                Spacer().frame(width: 16)
+            }
+            Spacer().frame(height: 11)
+        }
+        .aspectRatio(390.0/219.0, contentMode: .fit)
+        .background(Color.black)
+        .padding()
+    }
+}
+
+struct UpcomingVideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpcomingVideoView(item: MockUpcomingItem(), event: nil)
+    }
+}
+
+struct MockUpcomingItem: UpcomingItem {
+    var secCountDown: Int = 10
+    var imageURL: String = "https://placehold.co/144x75/png"
+    var headline: String = "獨家專訪｜用科技顛覆金融 李小加革新小店投資模式"
+    var timeCode: String = "22:22"
+}
---
+++ Tests/MobileDesignTests/ReelTests/UpcomingVideoViewTest.swift
@@ -0,0 +1,47 @@
+//
+//  ReelPagerTest.swift
+//
+//
+//  Created by James Kong on 10/9/2023.
+//
+
+import SnapshotTesting
+import SwiftUI
+import XCTest
+
+@testable import MobileDesign
+
+final class UpcomingVideoViewTest: XCTestCase {
+
+  override func setUpWithError() throws {
+  }
+
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+
+  func test_default_upcoming_video_snapshot() throws {
+    let rootView = sut
+    let vc = UIHostingController(rootView: rootView)
+    vc.overrideUserInterfaceStyle = .light
+
+    let expectation = expectation(description: "loading reels")
+    vc.viewDidLoad()
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+
+      if let view = vc.view {
+        assertSnapshot(matching: vc, as: .image(on: .iPhone13(.portrait)))
+      } else {
+        XCTFail("view not found")
+      }
+      expectation.fulfill()
+    }
+
+    wait(for: [expectation], timeout: 2)
+  }
+
+  var sut: some View {
+      UpcomingVideoView(item: MockUpcomingItem(), event: nil)
+  }
+
+}
