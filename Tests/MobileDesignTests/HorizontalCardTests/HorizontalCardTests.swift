07dc90fb8c9bc92217e3df5734a8a25bfbfb51a9
Felix Fok <felixfok@nmg.com.hk>
2023-10-20 18:48:56 +0800

    fix: updated snapshot test ref.

--- Tests/MobileDesignTests/HorizontalCardTests/HorizontalCardTests.swift
+++ Tests/MobileDesignTests/HorizontalCardTests/HorizontalCardTests.swift
@@ -40,7 +40,7 @@ final class HorizontalCardTests: XCTestCase {
     
     var sut: some View {
         HorizontalCard<CardData>(data: CardData(
-            imageURL: "https://placehold.co/133x75/png",
+            imageURL: "",
             headline: "獨家專訪｜用科技顛覆金融 李小加革新小店投資模式獨家專訪｜用科技顛覆金融 李小加革新小店投資模式",
             leadingFootnote: "4小時前",
             secondFootnote: "經人觀點",
