68ca28b4d900cb84cbc0cd9b5d5b6ec9c8e903c3
Felix Fok <felixfok@nmg.com.hk>
2023-10-20 17:09:50 +0800

    fix: updated snapshot test simulator

--- Tests/MobileDesignTests/PlaylistItemTests/PlaylistItemTests.swift
+++ Tests/MobileDesignTests/PlaylistItemTests/PlaylistItemTests.swift
@@ -42,7 +42,7 @@ final class PlaylistItemTests: XCTestCase {
 
     var sut: some View {
         PlaylistItem<CardData>(data: CardData(
-            imageURL: "",
+            imageURL: "https://placehold.co/358x200/png",
             headline: "獨家專訪｜用科技顛覆金融 李小加革新小店投資模式",
             leadingFootnote: "4小時前",
             secondFootnote: "經人觀點",
