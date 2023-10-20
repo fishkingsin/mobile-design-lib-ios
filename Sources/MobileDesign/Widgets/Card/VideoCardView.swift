c98fc305e7572baa297ea0747883d4e2c517a0c5
Felix Fok <felixfok@nmg.com.hk>
2023-10-20 18:30:35 +0800

    fix: use onTapGesture replace button

--- Sources/MobileDesign/Widgets/Card/VideoCardView.swift
+++ Sources/MobileDesign/Widgets/Card/VideoCardView.swift
@@ -15,37 +15,37 @@ public struct VideoCardView<Data>: View where Data: CardDisplayable & TimecodeDi
         self.onTabChanged = onTabChanged
     }
     public var body: some View {
-        Button(action: {
-            onTabChanged()
-        }) {VStack {
-            TopImageCardView(imageUrl: data.imageURL, imageWidth: nil, imageHeight: 200) {
-                // MARK: update placeholder
-                Rectangle()
-                    .fill(.gray)
-
-            } contentView: {
-                CardContentView(
-                    headline: data.headline,
-                    leadingFootnote: data.leadingFootnote,
-                    secondFootnote: data.secondFootnote
-                ).padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
-            } overlayView: {
-                CardTimecodeOverlayView(timecode: data.timecode).padding(EdgeInsets(top: 0, leading: 0, bottom: 4, trailing: 4))
-            }
-
-            .padding(12)
+        VStack {
+        TopImageCardView(imageUrl: data.imageURL,
+                         imageWidth: nil,
+                         imageHeight: 200) {
+            // MARK: update placeholder
             Rectangle()
-                .fill(ThemeManager.shared.currentTheme.colors.neutralGray5.color)
-                .frame(height: 2)
-        }}
-
+                .fill(.gray)
+        } contentView: {
+            CardContentView(
+                headline: data.headline,
+                leadingFootnote: data.leadingFootnote,
+                secondFootnote: data.secondFootnote
+            )
+            .padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
+        } overlayView: {
+            CardTimecodeOverlayView(timecode: data.timecode).padding(EdgeInsets(top: 0, leading: 0, bottom: 4, trailing: 4))
+        }
+        .padding(12)
+        Rectangle()
+            .fill(ThemeManager.shared.currentTheme.colors.neutralGray5.color)
+            .frame(height: 2)
+        }
+        .onTapGesture {
+            onTabChanged()
+        }
     }
 }
 
 struct VideoCardView_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
-
             Color.red
             VideoCardView<CardData>(data: CardData(
                 imageURL: "https://placehold.co/358x200/png",
