2b13a25112db302f6477279202426833e6a161c0
Joey Suen <joey_sun@chiefgroup.com.hk>
2023-11-07 16:46:33 +0800

    feat: add KingfisherWebP support

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.12.0"),
         .package(url: "https://github.com/fredyshox/PageView.git", .upToNextMajor(from: "1.5.0")),
         .package(url: "https://github.com/siteline/swiftui-introspect", from: "0.12.0"),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0")
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
+        .package(url: "https://github.com/yeatse/KingfisherWebP.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,6 +29,7 @@ let package = Package(
             dependencies: [
                 "PageView",
                 "Kingfisher",
+                "KingfisherWebP",
                 .product(name: "SwiftUIIntrospect", package: "swiftui-introspect")
             ],
             resources: [
